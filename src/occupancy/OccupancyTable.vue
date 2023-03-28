49d333144c246cf179967798d2a0468a1261bd7f
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-03-28 21:06:37 +0100

    scaffold out rough template structure for displaying floors on a seperate `<v-card>`; might turn this into its own component later too

--- src/occupancy/OccupancyTable.vue
+++ src/occupancy/OccupancyTable.vue
@@ -1,17 +1,25 @@
 <template>
-  <v-data-table :items="records.history" :headers="headers"/>
+  <div>
+    <v-card>
+      <p>My sidebar that lists all floors will go here...</p>
+    </v-card>
+    <br />
+    <v-card>
+      <v-data-table :items="records.history" :headers="headers" />
+    </v-card>
+  </div>
 </template>
 
 <script setup>
-import {useRecords} from '@/occupancy/records';
-import {computed} from 'vue';
+import { useRecords } from '@/occupancy/records';
+import { computed } from 'vue';
 
 const records = useRecords();
 const headers = computed(() => {
   return [
-    {text: 'When', value: 'time'},
-    {text: 'Floor', value: 'floor'},
-    {text: 'Occupancy', value: 'peopleCount', align: 'right'}
+    { text: 'When', value: 'time' },
+    { text: 'Floor', value: 'floor' },
+    { text: 'Occupancy', value: 'peopleCount', align: 'right' }
   ]
 })
 </script>
