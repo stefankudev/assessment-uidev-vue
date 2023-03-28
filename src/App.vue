60d0d3bb6bcdb2d64ee14c73de213e0d6a6bcfd1
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-03-29 00:51:29 +0100

    the poor contrast of the text was annoying

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
+<script setup>
+import OccupancyTable from '@/occupancy/OccupancyTable.vue';
+</script>
+
 <template>
   <v-app class="app-root">
     <v-app-bar app color="primary">
-      Vanti Front End Dev Assessment
+      <h1 id="app-title">Vanti Front End Dev Assessment</h1>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -11,6 +15,10 @@
   </v-app>
 </template>
 
-<script setup>
-import OccupancyTable from '@/occupancy/OccupancyTable.vue';
-</script>
+<style>
+#app-title {
+  font-size: 1rem;
+  font-weight: normal;
+  color: #ffffff;
+}
+</style>
