b7887ab301c016263ab600f5f2c7e7994faf286d
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-03-28 22:14:18 +0100

    add filtering functionality as sidebar dropdown; reorder component as `<script>`, `<template>`, `<style>`

--- src/occupancy/OccupancyTable.vue
+++ src/occupancy/OccupancyTable.vue
@@ -1,15 +1,3 @@
-<template>
-  <div>
-    <v-card>
-      <p>My sidebar that lists all floors will go here...</p>
-    </v-card>
-    <br />
-    <v-card>
-      <v-data-table :items="records.history" :headers="headers" />
-    </v-card>
-  </div>
-</template>
-
 <script setup>
 import { useRecords } from '@/occupancy/records';
 import { computed } from 'vue';
@@ -20,6 +8,52 @@ const headers = computed(() => {
     { text: 'When', value: 'time' },
     { text: 'Floor', value: 'floor' },
     { text: 'Occupancy', value: 'peopleCount', align: 'right' }
-  ]
-})
+  ];
+});
+
+const selectedFloor = computed({
+  get: () => records.selectedFloor,
+  set: value => {
+    records.selectedFloor = value;
+  }
+});
 </script>
+
+<template>
+  <section id="wrapper">
+    <aside>
+      <v-card class="card">
+        <!-- Some props I might also use here are "multiple chips closable-chips clearable" but then I would also need to change the state to an array of values -->
+        <v-select v-model="selectedFloor" :items="records.floors" label="Select a floor" />
+      </v-card>
+    </aside>
+    <main>
+      <v-card class="card">
+        <v-data-table :items="records.history" :headers="headers" />
+      </v-card>
+    </main>
+  </section>
+</template>
+
+<style lang="scss">
+#wrapper {
+  display: flex;
+  min-height: 30vh;
+  max-height: 90vh;
+}
+
+aside {
+  width: 30%;
+  padding-right: 1rem;
+  min-height: 100%;
+}
+
+main {
+  flex: 1;
+}
+
+.card {
+  padding: 1rem;
+  height: 100%;
+}
+</style>
